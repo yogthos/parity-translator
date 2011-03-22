f6f29834410c16e527e57b6b4ac795d1c41ff937
Ghast <yogthos@gmail.com>
2011-03-22 19:16:06 -0400

    code cleanup

--- src/translate.clj
+++ src/translate.clj
@@ -21,36 +21,36 @@
    
     (defn send-query [source, target, text]     
       (let [query (str url
-                    "?key=YOUR_KEY"                     
-                    "&source=" source
-                    "&target=" target
-                    "&q=" (URLEncoder/encode text "UTF-8"))
-            http-get (new HttpGet query)]       
-        (->> (get-response http-get)
-          json/decode-from-str
-          :data
-          :translations
-          first
-          :translatedText)))))
+                      "?key=YOUR_KEY" 
+                      "&source=" source
+                      "&target=" target
+                      "&q=" (URLEncoder/encode text "UTF-8"))]
+        
+        (->> (get-response (new HttpGet query))
+             json/decode-from-str
+             :data
+             :translations
+             first
+             :translatedText)))))
  
 
 (defn translate [langs text]
-  (reduce (fn [cur-text [from to]]            
-            (send-query from to cur-text))
+  (reduce (fn [cur-text [from to]] (send-query from to cur-text))
     text (partition 2 1 langs)))
 
 (defn -main [text from to & more]
   (init-client "https://www.googleapis.com/language/translate/v2")
   (let [langs (concat [from to] more [from])]
+    
     (println "translating through:" langs)
     (loop [last-text text
            cur-text (translate langs text)]
       (if (= last-text cur-text)
         (println "\noriginal:  " text
-          "\ntranslated:" cur-text)
+                 "\ntranslated:" cur-text)
         (do
           (println "\nbefore:" last-text
-            "\nafter: "  cur-text)
+                   "\nafter: "  cur-text)
           (recur cur-text (translate langs cur-text)))))))
 
-;(-main "Tear-drinking moths, probably the world's biggest assholes, sneak up on larger animals and poke them in the eyes until they cry so they can drink the tears." "en" "de" "fr")
+;(-main "There are also at least three species of tear-drinking bees, all from Thailand" "en" "de" "cs" "et")
